d40d10e883d6ac6215939ff6cf8ae6959b3b0707
Deftry <nikolajkozin15@gmail.com>
2023-12-07 22:23:10 +0700

    a

--- cogs/rp_utils.py
+++ cogs/rp_utils.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import datetime
 
 import json
@@ -10,6 +10,7 @@ OFFTOPIC_PREFIXES = ["//", "(("]
 class rp_utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.away_from_keyboard_checks.start()
 
     rpu = discord.SlashCommandGroup("рп_утилиты", "Утилиты для менеджмента РП процесса")
 
@@ -24,6 +25,10 @@ class rp_utils(commands.Cog):
         await ctx.followup.send(f"Будет удалено {len(messages)} сообщен{'ие' if len(messages) == 1 or len(messages) % 10 == 1 else 'ия' if len(messages) % 10 in (2, 3, 4) else 'ий' if len(messages) % 10 in (5, 6, 7, 8, 9, 0) else 'хз'}.", delete_after=10)
         for i in range(0, len(messages), 100):
             await ctx.channel.delete_messages(messages[i:i+100], reason=f"Удаление оффтоп сообщений в РП канале (префикс сообщения один из {OFFTOPIC_PREFIXES})")
+
+    @tasks.loop(seconds=300)
+    async def away_from_keyboard_checks():
+        pass
 
 def setup(bot):
     bot.add_cog(rp_utils(bot))
