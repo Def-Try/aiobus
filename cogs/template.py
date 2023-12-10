6718e7c28f76e805d9899d661e64a75064345dbd
Deftry <nikolajkozin15@gmail.com>
2023-12-10 23:44:36 +0700

    update template cog

--- cogs/template.py
+++ cogs/template.py
@@ -7,5 +7,12 @@ class template(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    cmds = discord.SlashCommandGroup("", "",
+        name_localizations=LOCALISATIONS["cog"]["template"]["command_group"]["name"],
+        description_localisations=LOCALISATIONS["cog"]["template"]["command_group"]["desc"])
+    @cmds.command(guild_ids=CONFIG["g_ids"],
+        name_localizations=LOCALISATIONS["cog"]["template"]["commands"]["cmd"]["name"],
+        description_localisations=LOCALISATIONS["cog"]["template"]["commands"]["cmd"]["desc"])
+
 def setup(bot):
     bot.add_cog(template(bot))
