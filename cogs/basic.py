0ab09b8a57b8950a0c245ebdc0ebbff4a22f37ab
CommandLine <71168720+Def-Try@users.noreply.github.com>
2024-01-20 22:52:44 +0700

    fix linting in basic cog

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,13 +1,11 @@
+import inspect
 import discord
 from discord.ext import commands
 from config import CONFIG
 from localisation import localise, DEFAULT_LOCALE
-import json
-import traceback
-import inspect
 
 
-class basic(commands.Cog):
+class Basic(commands.Cog):
     author = "googer_"
 
     def __init__(self, bot):
@@ -60,12 +58,12 @@ class basic(commands.Cog):
             ).format(mention=self.bot.user.mention),
         )
 
-        def do_commands(ctx, commands):
+        def do_commands(ctx, cmds):
             string = ""
 
-            def collect_commands(commands):
+            def collect_commands(to_collect):
                 to_do_commands = []
-                for command in commands:
+                for command in to_collect:
                     if isinstance(command, discord.SlashCommandGroup):
                         to_do_commands += collect_commands(command.walk_commands())
                     else:
@@ -82,18 +80,19 @@ class basic(commands.Cog):
                     name = do_name(ctx, command.parent) + " " + name
                 return name
 
-            commands = collect_commands(commands)
-            for command in commands[:5]:
+            cmds = collect_commands(cmds)
+            for command in cmds[:5]:
                 string += "`" + do_name(ctx, command) + "` - `" + command.name + "`\n"
-            if len(commands[5:]) > 0:
+            if len(cmds[5:]) > 0:
                 string += localise(
                     "cog.basic.answers.help.hidden", ctx.interaction.locale
-                ).format(amount=len(commands[5:]))
+                ).format(amount=len(cmds[5:]))
             return string.strip()
 
         for name, cog in self.bot.cogs.items():
             embed.add_field(
-                name=f"Cog `{name}` - {localise(f'cog.{cog.qualified_name}.info.brief', ctx.interaction.locale)}",
+                name=f"Cog `{name}` - " + \
+                  localise(f'cog.{cog.qualified_name}.info.brief', ctx.interaction.locale),
                 value=do_commands(ctx, cog.get_commands()),
                 inline=False,
             )
@@ -112,9 +111,9 @@ class basic(commands.Cog):
     def guess_cog(self, ctx, command):
         command_parts = command.split(" ")
 
-        def collect_commands(commands):
+        def collect_commands(to_collect):
             to_do_commands = []
-            for command in commands:
+            for command in to_collect:
                 if isinstance(command, discord.SlashCommandGroup):
                     to_do_commands += collect_commands(command.walk_commands())
                 else:
@@ -132,10 +131,10 @@ class basic(commands.Cog):
             return name
 
         for _, cog in self.bot.cogs.items():
-            for command in collect_commands(cog.walk_commands()):
-                if command.name == command_parts[0]:
+            for cmd in collect_commands(cog.walk_commands()):
+                if cmd.name == command_parts[0]:
                     return cog
-                if do_name(ctx, command) == " ".join(command_parts):
+                if do_name(ctx, cmd) == " ".join(command_parts):
                     return cog
 
     def find_command(self, ctx, command, cog):
@@ -318,9 +317,9 @@ class basic(commands.Cog):
                 string += "`" + do_name(ctx, command) + "` - `" + command.name + "`\n"
             return string.strip()
 
-        embed.add_field(name=f"Author", value="`" + cog.author + "`", inline=False)
+        embed.add_field(name="Author", value="`" + cog.author + "`", inline=False)
         embed.add_field(
-            name=f"Commands", value=do_commands(ctx, cog.get_commands()), inline=False
+            name="Commands", value=do_commands(ctx, cog.get_commands()), inline=False
         )
         await ctx.respond(embed=embed, ephemeral=True)
 
@@ -337,7 +336,8 @@ class basic(commands.Cog):
         )
         embed.add_field(
             name=localise("cog.basic.answers.ping.dwpl", ctx.interaction.locale),
-            value=f"{round(self.bot.latency*1000, 2)}{localise('cog.basic.answers.ping.unit', ctx.interaction.locale)}",
+            value=f"{round(self.bot.latency*1000, 2)}"
+              f"{localise('cog.basic.answers.ping.unit', ctx.interaction.locale)}",
             inline=False,
         )
 
@@ -407,4 +407,4 @@ class basic(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(basic(bot))
+    bot.add_cog(Basic(bot))
