70441100a3c32ffcffbf60da68b2401492c7baf0
Deftry <nikolajkozin15@gmail.com>
2024-02-06 01:56:00 +0700

    googer

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -462,7 +462,8 @@ YOUR LAWS:
                 g4f.Provider.AiChatOnline,
                 g4f.Provider.Bing,
                 g4f.Provider.Aura,
-                g4f.Provider.Llama2
+                g4f.Provider.Llama2,
+                g4f.Provider.Phind
             ]
             providers = [self.preferred_provider] + [
                 getattr(g4f.Provider, i)
