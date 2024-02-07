f5b3cf8f51c49bbdd7a0c0bf40786bd45daca285
Def-Try <Def-Try@users.noreply.github.com>
2024-02-07 11:28:02 +0000

    Apply automatic changes

--- cogs/googer_lenivy.py
+++ cogs/googer_lenivy.py
@@ -12,7 +12,10 @@ class GoogerIsLenivy(commands.Cog):
 
     @commands.Cog.listener("on_message")
     async def on_msg(self, message):
-        if 891289716501119016 in [u.id for u in message.mentions] and message.author != self.bot.user:
+        if (
+            891289716501119016 in [u.id for u in message.mentions]
+            and message.author != self.bot.user
+        ):
             await message.reply("лень")
 
 
