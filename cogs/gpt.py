5b7d31e4a08d88fcb36a9a90447175d75e32c8a9
Deftry <nikolajkozin15@gmail.com>
2024-02-07 21:29:45 +0700

    proxy

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -483,7 +483,7 @@ YOUR LAWS:
                 print("Trying provider", provider.__name__)
                 try:
                     result = await g4f.ChatCompletion.create_async(
-                        model="gpt-3.5-turbo", messages=smessages, provider=provider
+                        model="gpt-3.5-turbo", messages=smessages, provider=provider, proxy="http://159.65.245.255:80"
                     )
                 except Exception as e:
                     print("Provider " + provider.__name__, e)
