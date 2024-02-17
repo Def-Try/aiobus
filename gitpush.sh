c7fb56a43b9fd32e7c601bd1be186493e15c0a78
CommandLine <71168720+Def-Try@users.noreply.github.com>
2024-02-17 11:21:39 +0700

    imports-reorder воркфлоу

--- gitpush.sh
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "$1" &&
-git pull &&
-git push
---
+++ scripts/install-deps.sh
@@ -0,0 +1,11 @@
+python -m pip install --upgrade pip
+pip install pylint
+pip install py-cord
+pip install simpleeval
+pip install tinydb
+pip install aiohttp==3.6.2
+pip install nekosbest
+pip install --upgrade aiohttp
+pip install autopep8
+pip install git+https://github.com/psf/black
+pip install reorder-python-imports
