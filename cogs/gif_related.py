65d15dc43e7d6759d443408b105fc7e17f4978f1
Def-Try <Def-Try@users.noreply.github.com>
2024-02-24 16:11:12 +0000

    Apply automatic changes

--- cogs/gif_related.py
+++ cogs/gif_related.py
@@ -3,9 +3,9 @@ import io
 
 import aiohttp
 import discord
+import petpetgif
 from discord.ext import commands
 from nekosbest import Client
-import petpetgif
 
 from config import CONFIG
 from localisation import DEFAULT_LOCALE
