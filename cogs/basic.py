b304e342db956b5d62dd3b1b7e5cb9ae349f03d5
Def-Try <Def-Try@users.noreply.github.com>
2024-02-25 01:53:17 +0000

    Apply automatic changes

--- cogs/basic.py
+++ cogs/basic.py
@@ -147,7 +147,8 @@ class Basic(commands.Cog, name="basic"):
 
     @tasks.loop(seconds=1)
     async def activity(self):
-        if self.bot.is_closed(): return
+        if self.bot.is_closed():
+            return
         await self.bot.change_presence(activity=random.choice(activities)(self.bot))
 
     def cog_unload(self):
