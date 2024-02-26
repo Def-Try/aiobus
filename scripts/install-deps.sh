3929109e0a06026a446dd19ed3dc5071588abc11
CommandLine <71168720+Def-Try@users.noreply.github.com>
2024-02-26 14:08:05 +0700

    nekosbest обновили

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,9 +3,7 @@ pip install pylint
 pip install py-cord
 pip install simpleeval
 pip install tinydb
-pip install aiohttp==3.6.2
 pip install nekosbest
-pip install --upgrade aiohttp
 pip install colorama
 pip install termcolor
 pip install openai
