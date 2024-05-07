74e6d82937ddd55f370991516b8432188d20e2c6
Def-Try <root@googerlabs.io>
2024-05-07 18:48:45 +0700

    /util translate команда

--- cogs/util.py
+++ cogs/util.py
@@ -1,4 +1,5 @@
 import urllib.parse
+import aiohttp
 
 import discord
 from discord.ext import commands
@@ -35,7 +36,7 @@ class Utils(commands.Cog, name="util"):
                 "cog.util.commands.googlethatforyou.options.question.name"
             ),
             description=localise(
-                "cog.util.commands.fgooglethatforyou.options.question.desc",
+                "cog.util.commands.googlethatforyou.options.question.desc",
                 DEFAULT_LOCALE,
             ),
             description_localizations=localise(
@@ -48,7 +49,7 @@ class Utils(commands.Cog, name="util"):
                 "cog.util.commands.googlethatforyou.options.asker.name"
             ),
             description=localise(
-                "cog.util.commands.fgooglethatforyou.options.asker.desc", DEFAULT_LOCALE
+                "cog.util.commands.googlethatforyou.options.asker.desc", DEFAULT_LOCALE
             ),
             description_localizations=localise(
                 "cog.util.commands.googlethatforyou.options.asker.desc"
@@ -70,6 +71,69 @@ class Utils(commands.Cog, name="util"):
                 link=f"https://googlethatforyou.com/?q={question}", asker=asker.mention
             )
         )
+
+    @cmds.command(
+        guild_ids=CONFIG["g_ids"],
+        name_localizations=localise("cog.util.commands.translate.name"),
+        description_localizations=localise("cog.util.commands.translate.desc"),
+    )
+    async def translate(
+        self,
+        ctx: discord.ApplicationContext,
+        text: discord.Option(
+            str,
+            name_localizations=localise(
+                "cog.util.commands.translate.options.text.name"
+            ),
+            description=localise(
+                "cog.util.commands.translate.options.text.desc",
+                DEFAULT_LOCALE,
+            ),
+            description_localizations=localise(
+                "cog.util.commands.translate.options.text.desc"
+            ),
+        ),
+        target: discord.Option(
+            str,
+            name_localizations=localise(
+                "cog.util.commands.translate.options.target.name"
+            ),
+            description=localise(
+                "cog.util.commands.translate.options.target.desc", DEFAULT_LOCALE
+            ),
+            description_localizations=localise(
+                "cog.util.commands.translate.options.target.desc"
+            ),
+        ) = None,
+        source: discord.Option(
+            str,
+            name_localizations=localise(
+                "cog.util.commands.translate.options.source.name"
+            ),
+            description=localise(
+                "cog.util.commands.translate.options.source.desc", DEFAULT_LOCALE
+            ),
+            description_localizations=localise(
+                "cog.util.commands.translate.options.source.desc"
+            ),
+        ) = "auto",
+    ):
+        if target is None:
+            target = ctx.interaction.locale
+        await ctx.response.defer()
+        data = {}
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get("https://clients5.google.com/translate_a/t", timeout=5, params={"client": "dict-chrome-ex", "dt": "t", "tl": target, "sl": source, "q": text}) as response:
+                    data = await response.json()
+        except TimeoutError:
+            await ctx.followup.send(localise("cog.util.answers.translate.error", ctx.interaction.locale))
+            return
+        except Exception as e:
+            await ctx.followup.send(localise("cog.util.answers.translate.error", ctx.interaction.locale))
+            return
+        await ctx.followup.send(localise("cog.util.answers.translate.translated", ctx.interaction.locale).format(target=target, source=data[0][1], translated=data[0][0]))
+            
 
 
 def setup(bot):
