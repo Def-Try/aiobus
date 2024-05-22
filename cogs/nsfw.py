f3ea7b6c8437c1d13fc51fd6cf1dd27c785dac21
googer <googer@mindurka.fun>
2024-05-22 17:19:51 +0000

    Apply automatic reformat

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -150,7 +150,9 @@ class NSFW(commands.Cog, name="nsfw"):
             post = random.choice(_posts)
 
         async with aiohttp.ClientSession() as session:
-            status = await download_file(session, _provider.get_img_url(post), media=True)
+            status = await download_file(
+                session, _provider.get_img_url(post), media=True
+            )
             if status["error"]:
                 await ctx.followup.send(status["error"])
                 return
@@ -210,7 +212,9 @@ class NSFW(commands.Cog, name="nsfw"):
         files = []
         async with aiohttp.ClientSession() as session:
             for post in posts:
-                status = await download_file(session, _provider.get_img_url(post), media=True)
+                status = await download_file(
+                    session, _provider.get_img_url(post), media=True
+                )
                 if status["error"]:
                     continue
                 dest = io.BytesIO(status["data"])
